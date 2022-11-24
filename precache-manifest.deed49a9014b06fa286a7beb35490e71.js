bb2634ac5002166c2b7fdbde81800e18e7729748
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2022-11-25 01:54:25 +0300

    Updates

---
+++ precache-manifest.deed49a9014b06fa286a7beb35490e71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d1667e73c95e1e09eaa",
+    "url": "/quiz-react/static/css/main.e0f251e2.chunk.css"
+  },
+  {
+    "revision": "5d1667e73c95e1e09eaa",
+    "url": "/quiz-react/static/js/main.5d1667e7.chunk.js"
+  },
+  {
+    "revision": "7f5b212f390b8f4d26dd",
+    "url": "/quiz-react/static/js/1.7f5b212f.chunk.js"
+  },
+  {
+    "revision": "910e6c7a120fea6a1c88",
+    "url": "/quiz-react/static/js/runtime~main.910e6c7a.js"
+  },
+  {
+    "revision": "d550f629fd24b15b22e995b8c0f1c714",
+    "url": "/quiz-react/static/media/47fbb68a37492662cd74715d7787a212.d550f629.jpg"
+  },
+  {
+    "revision": "3de6791c35b565e7a262bdaf415b5082",
+    "url": "/quiz-react/index.html"
+  }
+];
